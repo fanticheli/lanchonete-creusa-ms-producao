4d22e60b0879a5fed6f8baea01772430678db99f
Cassio Caue <cassio3620@gmail.com>
2024-01-25 23:23:15 -0300

    feat: alterando config jest

--- jest.config.ts
+++ jest.config.ts
@@ -32,16 +32,16 @@ const config: Config = {
   // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
- coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "src/api",
-    "src/external",
-    "src/interfaces",
-    "src/index",
-    "src/controllers",
-    "src/entities",
-  ],
+ //coveragePathIgnorePatterns: [
+  //   "/node_modules/",
+  //   "src/api",
+  //   "src/external",
+  //   "src/interfaces",
+  //   "src/index",
+  //   "src/controllers",
+  //   "src/entities",
   // ],
+ // ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
@@ -57,10 +57,10 @@ const config: Config = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 
